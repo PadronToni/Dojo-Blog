2b6223ac65acb6e4ae557c8b395cb50602a73971
Simone Boccuzzi <mughetsu2@gmail.com>
2022-07-03 18:02:29 +0200

    added flex-flow classes

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -2,6 +2,13 @@ const Create = () => {
 	return ( 
 		<div className="create">
 			<h2>Create component works</h2>
+			<form action="" className="stack flex-col-container">
+				<div>
+					<label htmlFor="title">Title</label>
+					<input type="text" name="title" id="title" />
+				</div>
+
+			</form>
 		</div>
 	 );
 }
